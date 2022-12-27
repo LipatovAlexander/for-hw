f37ef2c9e6fca8bd76cb5104a5368dc392cab914
LipatovAlexander <lipatov.work@bk.ru>
2022-12-27 14:14:35 +0300

    use colorsys

--- part1/hw9/utils.py
+++ part1/hw9/utils.py
@@ -1,16 +1,16 @@
-﻿import math
+﻿import colorsys
+import math
 import numpy as np
-from HSV.hsv import hsv_to_rgb
 
 
 def hsv2rgb(h, s, v):
     h /= 360.
     s /= 100.
     v /= 100.
-    r, g, b = hsv_to_rgb(h, s, v)
+    r, g, b = colorsys.hsv_to_rgb(h, s, v)
     return round(r * 255), round(g * 255), round(b * 255)
-    
-    
+
+
 def pol2cart(radius, angle):
     x = radius * math.cos(math.radians(angle))
     y = radius * math.sin(math.radians(angle))
@@ -36,6 +36,5 @@ def points_in_circle(radius, x0=0, y0=0):
     x_ = np.arange(x0 - radius - 1, x0 + radius + 1, dtype=int)
     y_ = np.arange(y0 - radius - 1, y0 + radius + 1, dtype=int)
     x, y = np.where((x_[:,np.newaxis] - x0)**2 + (y_ - y0)**2 <= radius**2)
-    # x, y = np.where((np.hypot((x_-x0)[:,np.newaxis], y_-y0)<= radius)) # alternative implementation
     for x, y in zip(x_[x], y_[y]):
         yield x, y
