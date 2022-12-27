cb49772489d85d2974034d4a876395d7b1d6f8a0
LipatovAlexander <lipatov.work@bk.ru>
2022-12-27 14:07:40 +0300

    hw9

---
+++ part1/hw9/hw9.py
@@ -0,0 +1,70 @@
+﻿import cv2
+import numpy as np
+from utils import hsv2rgb, pol2cart, cart2pol, points_in_circle
+
+window_name = 'HSV Color Palette'
+color_window_name = 'Selected color'
+win_height = 300
+win_width = 300
+circle_center_x = 150
+circle_center_y = 150
+circle_radius = 140
+
+selected_h = 0
+selected_s = 0
+selected_v = 0
+
+color_img = np.zeros((200, 200, 3), np.uint8)
+img = np.zeros((win_height,win_width,3), np.uint8)
+
+def get_HSV(x, y):
+    s, h = cart2pol(x - circle_center_x, circle_center_y - y)
+    s = min(s, circle_radius) * (100 / circle_radius)
+    s, h = round(s), round(h)
+    return h, s, selected_v
+
+def draw_palette():
+    global selected_v
+    for x, y in points_in_circle(circle_radius, circle_center_x, circle_center_y):
+        if x == 265 and y == 75:
+            pass
+        h, s, v = get_HSV(x, y)
+        r, g, b = hsv2rgb(h, s, selected_v)
+        img[y, x] = [b, g, r]
+        cv2.imshow(window_name, img)
+
+
+def draw_color():
+    r, g, b = hsv2rgb(selected_h, selected_s, selected_v)
+    color_img[:] = [b, g, r]
+    cv2.imshow(color_window_name, color_img)
+
+
+def print_color():
+    print("HSV: ({}, {}, {})".format(selected_h, selected_s, selected_v))
+    r, g, b = hsv2rgb(selected_h, selected_s, selected_v)
+    print("RGB: ({}, {}, {})".format(r, g, b))
+
+
+def on_mouseclick(event, x, y, flags, param):
+    global selected_h, selected_s, selected_v
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        selected_h, selected_s, v = get_HSV(x, y)
+        draw_color()
+        print_color()
+
+
+def on_trackbar(x):
+    global selected_v
+    selected_v = x
+    draw_palette()
+    draw_color()
+
+
+cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
+cv2.createTrackbar('S', window_name, 100, 100, on_trackbar)
+cv2.setMouseCallback(window_name, on_mouseclick)
+draw_palette()
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ part1/hw9/utils.py
@@ -0,0 +1,41 @@
+﻿import math
+import numpy as np
+from HSV.hsv import hsv_to_rgb
+
+
+def hsv2rgb(h, s, v):
+    h /= 360.
+    s /= 100.
+    v /= 100.
+    r, g, b = hsv_to_rgb(h, s, v)
+    return round(r * 255), round(g * 255), round(b * 255)
+    
+    
+def pol2cart(radius, angle):
+    x = radius * math.cos(math.radians(angle))
+    y = radius * math.sin(math.radians(angle))
+    return x, y
+
+
+def cart2pol(x, y):
+    radius = math.sqrt(x * x + y * y)
+    theta = math.atan(y / x)
+    theta = 180 * theta / math.pi
+    if np.isnan(theta):
+        theta = 0
+    if x < 0:
+        theta = 180 + theta
+    if x > 0 and y < 0:
+        theta = 270 + (90 + theta)
+    if theta < 0:
+        theta = 360 + theta
+    return radius, theta
+
+
+def points_in_circle(radius, x0=0, y0=0):
+    x_ = np.arange(x0 - radius - 1, x0 + radius + 1, dtype=int)
+    y_ = np.arange(y0 - radius - 1, y0 + radius + 1, dtype=int)
+    x, y = np.where((x_[:,np.newaxis] - x0)**2 + (y_ - y0)**2 <= radius**2)
+    # x, y = np.where((np.hypot((x_-x0)[:,np.newaxis], y_-y0)<= radius)) # alternative implementation
+    for x, y in zip(x_[x], y_[y]):
+        yield x, y
